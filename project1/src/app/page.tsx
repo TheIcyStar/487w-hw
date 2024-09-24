92fe787527d16557ac59d335abcb3faebd2d09ae
TheIcyStar <TheIcyStar@users.noreply.github.com>
2024-09-24 00:34:12 -0400

    User add/update

--- project1/src/app/page.tsx
+++ project1/src/app/page.tsx
@@ -7,6 +7,7 @@ import { Prisma } from "@prisma/client"
 import { UserTable } from "./_components/usertable"
 import { SwipeTable } from "./_components/swipestable"
 import { SwipeForm } from "./_components/swipeform" 
+import { UserForm } from "./_components/userform"
 
 export default async function Home() {
   // const [dateFilter, setDateFilter] = React.useState<Date | undefined>()
@@ -40,46 +41,12 @@ export default async function Home() {
 
           <div>
             <SwipeForm></SwipeForm>
-
-            {/* <div className="bg-sky-100 p-5 rounded-md drop-shadow-lg ">
-              <p className="text-xl font-bold">Manual SUN Lab sign in</p>
-              <form>
-                <label htmlFor="student-id">Student ID: </label>
-                <input type="number" id="student-id" name="studentid"></input>
-
-                <input type="submit" value="Send ID to log" className="block bg-yellow-300 rounded-lg px-3 py-1 mt-3 hover:bg-yellow-500 hover:cursor-pointer"></input>
-              </form>
-            </div> */}
-
-
-            <div className="bg-sky-100 p-5 rounded-md drop-shadow-lg mt-5">
-              <p className="text-xl font-bold">Add/update a user</p>
-              {/* <form>
-                <label htmlFor="student-id">Student ID: </label>
-                <input type="number" id="student-id" name="studentid"></input>
-
-                <div>
-                  <label htmlFor="role">Role: </label>
-                  <select id="role" name="role">
-                    <option value="Student">Student</option>
-                    <option value="Faculty">Faculty</option>
-                    <option value="Staff">Staff</option>
-                    <option value="Janitor">Janitor</option>
-                  </select>
-                </div>
-
-                <div>
-                  <label htmlFor="active">Active: </label>
-                  <input type="checkbox" id="active" name="active" checked></input>
-                </div>
-
-                <input type="submit" value="Apply" className="block bg-yellow-300 rounded-lg px-3 py-1 mt-3 hover:bg-yellow-500 hover:cursor-pointer"></input>
-              </form> */}
-            </div>
+            <UserForm></UserForm>
+            
           </div>
 
         </div>
       </main>
     </HydrateClient>
-  );
+  )
 }
